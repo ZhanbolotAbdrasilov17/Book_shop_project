5c2782c20c1582c7971b415231fdd11d5fae61dc
Zhanbolot Abdrasilov <zhanbolot19971997@gmail.com>
2022-02-11 00:01:23 +0600

    js_code_and_adding_fitch

--- bookmaster/urls.py
+++ bookmaster/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name="store"),
-    path('card/', views.card, name="card"),
+    path('cart/', views.cart, name="cart"),
     path('checkout', views.checkout, name="checkout"),
+
+    path('update_item/', views.updateItem, name="update_item"),
 ]
